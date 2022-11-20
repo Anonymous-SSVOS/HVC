da59bfcb1a0c2f511dbec1747c75849b7d002ee3
Anonymous-SSVOS <116424359+Anonymous-SSVOS@users.noreply.github.com>
2022-11-20 18:59:24 +0800

    Add files via upload

---
+++ core/convert_pretrain.py
@@ -0,0 +1,19 @@
+import torch
+from collections import OrderedDict
+
+print(f"Loading checkpoint")
+state_dict = torch.load('./pretrain_ckpt/hvc.pth', map_location='cpu')
+
+new_ckpt = OrderedDict()
+
+
+# baseline
+for k, v in state_dict['model'].items():
+    if k.startswith('online_encoder.'):
+        new_v = v
+        new_k = k.replace('online_encoder.', '')
+        new_ckpt[new_k] = new_v
+    
+# print(new_ckpt)
+torch.save(new_ckpt, './checkpoints/hvc.pth')
+
